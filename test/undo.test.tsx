fe395a33275725829d811afeb2407b5632a29d1f
Mark Murray <mark@murray.xyz>
2019-01-19 22:41:23 +0000

    update undo and redo test to use input

--- test/undo.test.tsx
+++ test/undo.test.tsx
@@ -75,87 +75,56 @@ const renderApp = () => {
 }
 
 test('can undo and redo', () => {
-  const { useSuperState, Provider } = createSuperState(reducers, initialState)
+  const { undoButton, redoButton, getText, keyPress } = renderApp()
 
-  const Counter = () => {
-    const { actions, state, undo, redo, canUndo, canRedo } = useSuperState()
-
-    return (
-      <div>
-        <button
-          onClick={() => actions.incrementWithUndo()}
-          data-testid="increment"
-        >
-          Clicked {state.count} time(s)
-        </button>
-        <button data-testid="undo" onClick={undo}>
-          Can undo: {`${canUndo}`}
-        </button>
-        <button data-testid="redo" onClick={redo}>
-          Can redo: {`${canRedo}`}
-        </button>
-      </div>
-    )
-  }
-
-  const { getByText, getByTestId } = render(
-    <Provider>
-      <Counter />
-    </Provider>,
-  )
-
-  const incrementButton = getByTestId('increment')
-  const undoButton = getByTestId('undo')
-  const redoButton = getByTestId('redo')
-
-  expect(incrementButton.textContent).toBe('Clicked 0 time(s)')
+  expect(getText()).toBe('')
   expect(undoButton.textContent).toBe('Can undo: false')
   expect(redoButton.textContent).toBe('Can redo: false')
 
-  fireEvent.click(incrementButton)
+  keyPress('a')
 
-  expect(incrementButton.textContent).toBe('Clicked 1 time(s)')
+  expect(getText()).toBe('a')
   expect(undoButton.textContent).toBe('Can undo: true')
   expect(redoButton.textContent).toBe('Can redo: false')
 
-  fireEvent.click(incrementButton)
+  keyPress('b')
 
-  expect(incrementButton.textContent).toBe('Clicked 2 time(s)')
+  expect(getText()).toBe('ab')
   expect(undoButton.textContent).toBe('Can undo: true')
   expect(redoButton.textContent).toBe('Can redo: false')
 
   fireEvent.click(undoButton)
 
-  expect(incrementButton.textContent).toBe('Clicked 1 time(s)')
+  expect(getText()).toBe('a')
   expect(undoButton.textContent).toBe('Can undo: true')
   expect(redoButton.textContent).toBe('Can redo: true')
 
   fireEvent.click(undoButton)
 
-  expect(incrementButton.textContent).toBe('Clicked 0 time(s)')
+  expect(getText()).toBe('')
   expect(undoButton.textContent).toBe('Can undo: false')
   expect(redoButton.textContent).toBe('Can redo: true')
 
   fireEvent.click(undoButton)
-  expect(incrementButton.textContent).toBe('Clicked 0 time(s)')
+  expect(getText()).toBe('')
   expect(undoButton.textContent).toBe('Can undo: false')
   expect(redoButton.textContent).toBe('Can redo: true')
 
   fireEvent.click(redoButton)
 
-  expect(incrementButton.textContent).toBe('Clicked 1 time(s)')
+  expect(getText()).toBe('a')
   expect(undoButton.textContent).toBe('Can undo: true')
   expect(redoButton.textContent).toBe('Can redo: true')
 
   fireEvent.click(redoButton)
 
-  expect(incrementButton.textContent).toBe('Clicked 2 time(s)')
+  expect(getText()).toBe('ab')
   expect(undoButton.textContent).toBe('Can undo: true')
   expect(redoButton.textContent).toBe('Can redo: false')
 
   fireEvent.click(redoButton)
 
-  expect(incrementButton.textContent).toBe('Clicked 2 time(s)')
+  expect(getText()).toBe('ab')
   expect(undoButton.textContent).toBe('Can undo: true')
   expect(redoButton.textContent).toBe('Can redo: false')
 })
